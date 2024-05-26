2f5e246ce769ad7973b54ac943e0c8d0a53ad9e0
MD SADAB SAQIB <sadabnepal1993@gmail.com>
2024-05-26 11:54:41 +1000

    added env based files

---
+++ tests/env/manager.ts
@@ -0,0 +1,12 @@
+import { config } from 'dotenv';
+import { join } from 'path';
+
+const environment = process.env.ENV;
+if (!environment) throw new Error('please pass the environment variable. Options: | dev | stage | prod |');
+
+config({ path: join(process.cwd(), 'tests', 'env', `${environment}.env`) });
+
+export const ENV = {
+    BASEURL: process.env.BASEURL,
+    ENDPOINT: '/v2/graphql'
+};
